6d4f42db52ac97e79ad8af2052323edee31891e2
jfranklin12 <j.franks725@gmail.com>
2022-08-01 16:17:52 -0400

    Model Degree layout created

---
+++ models/Degree.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Degree extends Model {}
+
+Degree.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    degree_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    score_value: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+}, 
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modeName: "Degree",
+});
+
+module.exports = Degree;
