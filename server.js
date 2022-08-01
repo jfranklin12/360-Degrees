a6670efd4fc6f74d3b0e016c1470224ede06cc55
jfranklin12 <j.franks725@gmail.com>
2022-08-01 09:58:00 -0400

    connected to mysql database with sequelize

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const sequelize = require("./config/connection");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Listening on PORT # http://localhost:3001");
+  });
+});
