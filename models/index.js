c3b54b03b9087effcd85acd904b3489f257afe57
jfranklin12 <j.franks725@gmail.com>
2022-08-02 10:04:21 -0400

    Model index.js created

--- models/index.js
+++ models/index.js
@@ -3,4 +3,28 @@ const Review = require("./Review");
 const School = require("./School");
 const Degree = require("./Degree");
 
-// User.hasMany(P)
+User.hasMany(Review, {
+    foreignKey: "user_id",
+});
+
+Review.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Degree.hasMany(Review, {
+    foreignKey: "degree_id",
+});
+
+Review.belongsTo(Degree, {
+    foreignKey: "degree_id",
+});
+
+School.hasMany(Review, {
+    foreignKey: "school_id",
+});
+
+Review.belongsTo(School, {
+    foreignKey: "school_id",
+});
+
+module.exports = { User, Review, School, Degree};
