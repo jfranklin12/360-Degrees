870e11ed9b6ce15b6c9b293e516ce45e280025fc
chromodyne <dsinitiere@outlook.com>
2022-08-02 07:34:30 -0700

    Basic API routing structure implemented.

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const degreeRoutes = require("./degree-routes");
+const reviewRoutes = require("./review-routes");
+const userRoutes = require("./user-routes");
+
+//TODO: Probably won't need this one.
+//router.use("/user", userRoutes);
+
+router.use("/review", reviewRoutes);
+//router.use("/degree", degreeRoutes);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,13 @@
 const router = require("express").Router();
 
 const homeRoutes = require("./homeRoutes");
-//const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+    res.send("<h1>This is not the route you're looking for...</h1>")
+  });
 
 module.exports = router;
