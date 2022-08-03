dcb5db61d3cb6eb7333200cde8c78081f3877664
jfranklin12 <j.franks725@gmail.com>
2022-08-03 18:10:54 -0400

    const created to get values from review.handlebars in degree.js, script tag and submit button added to review.handllebars

---
+++ public/js/degree.js
@@ -0,0 +1,19 @@
+const newReview = async (event) => {
+    event.preventDefault();
+
+    const college = document.getElementById("college").value.trim();
+    const degree = document.getElementById("degree").value.trim();
+    const score_1 = document.getElementById("score_1");
+    const score_1_value = score_1.options[score_1.selectedIndex].score_1_value;
+    const score_2 =document.getElementById("score_2");
+    const score_2_value = score_2.options[score_2.selectedIndex].score_2_value;
+    const comment = document.getElementById("comment");
+
+    console.log(college);
+    console.log(degree);
+    console.log(score_1_value);
+    console.log(score_2_value);
+    console.log(comment);
+
+
+}
