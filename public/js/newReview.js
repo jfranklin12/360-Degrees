13dc7e275aa0bd5002f80c23171e27c368726b9a
jfranklin12 <j.franks725@gmail.com>
2022-08-03 18:56:30 -0400

    degree.js changed to newReview.js and code finished for newReviewHandler

--- public/js/newReview.js
+++ public/js/newReview.js
@@ -1,11 +1,11 @@
-const newReview = async (event) => {
+const newReviewHandler = async (event) => {
     event.preventDefault();
 
     const college = document.getElementById("college").value.trim();
     const degree = document.getElementById("degree").value.trim();
-    const score_1 = document.getElementById("score_1");
+    const score_1 = document.getElementById("score-1");
     const score_1_value = score_1.options[score_1.selectedIndex].score_1_value;
-    const score_2 =document.getElementById("score_2");
+    const score_2 =document.getElementById("score-2");
     const score_2_value = score_2.options[score_2.selectedIndex].score_2_value;
     const comment = document.getElementById("comment");
 
@@ -16,20 +16,19 @@ const newReview = async (event) => {
     console.log(comment);
 
     if(degree && score_1_value && score_2_value && comment) {
-        const response = await fetch("/api/review", {
+        const response = await fetch("/api/review/new", {
             method: "POST",
             body: JSON.stringify({ degree, score_1_value, score_2_value, comment}),
-            headers: {
-                // need to look up
-            },
+            headers: { "Content-Type": "application/json"},
         });
 
         if(response.ok) {
-            document.location.replace("/degree");
+            document.location.replace("/review/new");
         } else {
             alert("Something went wrong!");
         }
     }
-
-
 };
+
+document.getElementById("new-review-submit-button", newReviewHandler);
+
