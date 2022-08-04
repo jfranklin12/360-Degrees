49e1abecc06f76b4a255ef1da044ed690b8ec94b
MLyjak1 <mlyjak@gmail.com>
2022-08-04 10:16:50 -0400

    Fixed issuews with dropdowns

--- public/js/createReview.js
+++ public/js/createReview.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll("select");
+    console.log(elems);
     var instances = M.FormSelect.init(elems);
     console.log(instances);
 });
